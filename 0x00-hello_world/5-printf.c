6a513d14713cedf4489da644e497448ee2646912
Debbby1995 <debbytomi42@gmail.com>
2022-10-13 20:37:13 -0700

    My mind

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main-entry point for the program.
+* Return:0 if no errors, return non zeroif errors
+*/
+int main(void)
+{
+  printf(""with proper grammar, but the outcomes is a piece of art,\n");
+  return (0)
+}
