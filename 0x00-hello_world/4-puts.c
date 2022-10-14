a5cc32efee79240f4b32d70ce74655b6846f3f10
Debbby1995 <debbytomi42@gmail.com>
2022-10-13 23:45:51 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stdio>.h
+#include <stdio.h>
 /**
-*
-*main- Serves as the entry pointfor the program
-*Return: By default returns zero if no erros otherwise returns non-zero value.
-*/
+ ** main - entry point
+ **
+ ** Return: Always 0
+ **/
 int main(void)
 {
-  put("\"Programming is like building a multilingual puzzle");
-  return (0)
+puts("\"Programming is like building a multilingual puzzle");
+return (0)
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,6 @@
 */
 int main(void)
 {
-  printf(""with proper grammar, but the outcomes is a piece of art,\n");
-  return (0)
+printf("with proper grammar, but the outcomes is a piece of art,\n");
+return (0);
 }
