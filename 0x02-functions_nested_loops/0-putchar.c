6c46ad3b5bbc8391a95d176b4c40ccba8e802fc0
Debbby1995 <debbytomi42@gmail.com>
2022-10-19 14:35:51 -0700

    My commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,19 @@
 #include "main.h"
-
 /**
- * main - print _putchar
- * is written by deborah
- * Return: 0
+ * main - Entry po
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+	char *sh = "Main"
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+
 return (0);
 }
